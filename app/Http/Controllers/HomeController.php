3696e8cc63bb121f1de272e5ce4da0eb61480d9f
ervantonius <ervantonius@gmail.com>
2021-01-12 13:08:57 +0700

    list all positions job

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,64 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $input = $request->only([
+            'page',
+            'description',
+            'location',
+            'type_time'
+        ]);
+
+        if (!empty($input['type_time'])) {
+            $input['full_time'] = 'true';
+        }
+
+        $result = $this->list_positions(array_filter($input));
+
+        if (!empty($result) && is_array($result)) {
+	        return view('home', [
+	        	'data' => $result,
+                'is_able_next_page' => $this->is_able_next_page(array_filter($input))
+	        ]);
+        } else {
+            return view('home')->withErrors([
+            	'message' => 'There\'s something wrong with our data.'
+            ]);
+        }
+    }
+
+    private function list_positions($query = [])
+    {
+        $url = 'https://jobs.github.com/positions.json';
+        
+        if (!empty($query)) {
+            $url .= '?'.http_build_query($query);
+        }
+
+        $headers = array(
+            'Content-Type: application/x-www-form-urlencoded'
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $result = json_decode(curl_exec($ch));
+
+        return $result;
+    }
+
+    private function is_able_next_page($query = [])
+    {
+        if (!empty($query['page'])) {
+            $query['page'] = intval($query['page']) + 1;
+        } else {
+            $query['page'] = 2;
+        }
+
+        return (count($this->list_positions($query))) ? $query['page'] : 0;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,31 @@
 
 @section('content')
 <div class="container">
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Load data error!</strong> {{ $errors->first() }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <form>
+            <form action="{{ route('home') }}">
                 <div class="form-row">
                     <div class="form-group col-md-4">
                         <label for="description">Job Description</label>
-                        <input type="text" class="form-control" id="description" placeholder="Field by title, benefits, companies, expertise">
+                        <input type="text" class="form-control" name="description" id="description" placeholder="Field by title, benefits, companies, expertise" value="{{ \Request::input('description') ?? '' }}">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="location">Location</label>
-                        <input type="text" class="form-control" id="location" placeholder="Field by city, state, zip code or country">
+                        <input type="text" class="form-control" name="location" id="location" placeholder="Field by city, state, zip code or country" value="{{ \Request::input('location') ?? '' }}">
                     </div>
                     <div class="form-group col-md-3">
                         <label for="type_time">Job Type</label>
                         <select name="type_time" class="form-control" id="type_time">
                             <option value="">All</option>
-                            <option value="full_time">Full Time</option>
+                            <option value="full_time" {{ \Request::input('type_time') == 'full_time' ? 'selected=""' : '' }}>Full Time</option>
                         </select>
                     </div>
                     <div class="form-group col-md-1">
@@ -27,20 +35,54 @@
                     </div>
                 </div>
             </form>
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            @if (!empty($data))
+                <div class="row">
+                    @php
+                        $no = 0;
+                    @endphp
+                    @foreach ($data as $lowongan)
+                        @php
+                            $no++;
+                        @endphp
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                            <div class="col">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <strong>{{ $lowongan->title }}</strong>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="{{ $lowongan->company_url ?? '#' }}" target="_blank">{{ $lowongan->company }}</a>
+                                            <p>
+                                                <small>
+                                                    <u>{{ $lowongan->location }}</u> - <strong>{{ $lowongan->type }}</strong>
+                                                </small>
+                                            </p>
+                                        </h5>
+                                        <p class="card-text text-right"><small class="text-muted">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($lowongan->created_at))->diffForHumans() }}</small></p>
+                                    </div>
+                                </div>
+                            </div>
 
-                    You are logged in!
+                        @if ($no % 3 == 0)
+                        <div class="w-100 mb-3"></div>
+                        @endif
+                    @endforeach
                 </div>
-            </div>
+            @endif
+            @if (!empty($is_able_next_page))
+                @php
+                    $input = \Request::all();
+                    $input['page'] = $is_able_next_page;
+                @endphp
+                <a href="{{ route('home', $input) }}" class="btn btn-success btn-block mt-4" id="load_mode">Load more</a>
+            @endif
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+</script>
 @endsection
